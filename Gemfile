f32b07d02f3e5a8b980121624a7060ed60ba1e4d
natepost <nathanielpost@gmail.com>
2018-02-03 11:35:36 -0500

    add bootstrap and experiment controller scaffold

--- Gemfile
+++ Gemfile
@@ -10,8 +10,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 
 
-
-# gem 'bootstrap', '~> 4.0.0'
+ gem 'bootstrap', '~> 4.0.0'
+ gem 'jquery-rails'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
---
+++ test/system/experiments_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class ExperimentsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit experiments_url
+  #
+  #   assert_selector "h1", text: "Experiment"
+  # end
+end
